01bf0824dbf021b86328998b14e459bd7b892a8d
Thuraitun <thuraitun64@gmail.com>
2023-07-01 23:44:27 +0630

    second commit

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,24 @@
 
     <button @click="showPopup = true">Show Popup</button>
     <Popup v-show="showPopup" @close="closePopup" />
+    <br><br>
+    <Input v-model="name" />
+    <br><br>
+    <Card></Card>
+    <Card>Card Content 1 </Card>
+    <Card><h2>Card Content 2</h2></Card>
+    <Card><img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"></Card>
+    <Card>
+      <template v-slot:header>
+        <h2>Header</h2>
+      </template>
+      <template v-slot:default>
+        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg">
+      </template>
+      <template v-slot:footer>
+        <button>View Details</button>
+      </template>
+    </Card>
   </div>
 </template>
 
@@ -19,6 +37,8 @@
   import Article from "./components/Article.vue"
   import ComponentC from "./components/ComponentC.vue"
   import Popup from "./components/Popup.vue"
+  import Input from "./components/Input.vue"
+  import Card from "./components/Card.vue"
 
   export default {
     name: "App",
@@ -27,6 +47,8 @@
       Article,
       ComponentC,
       Popup,
+      Input,
+      Card,
     },
     data() {
       return {
@@ -52,5 +74,18 @@
 <style scoped>
   #app {
     text-align: center;
+  }
+  img{
+    width: 200px;
+  }
+  h2 {
+    text-align: center;
+    color:blue;
+  }
+  button {
+    padding: 7px 10px;
+    background-color: coral;
+    color: #fff;
+    border-radius: 8px;
   }
 </style>
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h2>Popup Component</h2>
-        <button @click="$emit('close', 'Thurai')">Popup Close</button>
+        <input type="text" v-model="name">
+        <button @click="$emit('close', name)">Popup Close</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Popup',
-        emits: ['close'],
+        emits: {
+            close: (name) => {
+                if(!name) {
+                    return false
+                } else {
+                    return true 
+                }
+            }
+        },
+        data() {
+            return {
+                name: '',
+            }
+        },
     }
 </script>
 
